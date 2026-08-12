0ceaab548c39cad5796aa51f9025a8fa80d69e86
carlosjimenohernandez <jimenohernandezcarlos@gmail.com>
2026-08-12 12:56:26 +0200

    El sectionsMap del dev/settings.js pasa a publicable y reusable en producción opcionalmente + test

--- dist/src/lib/dev-binary-v6.dist.js
+++ dist/src/lib/dev-binary-v6.dist.js
@@ -3501,15 +3501,17 @@
                 try {
                     const fs = require("fs");
                     const settingsAsyncFactory = require(filepath);
-                    const settingsData = await settingsAsyncFactory({
+                    const settingsData = typeof settingsAsyncFactory === "function" ? await settingsAsyncFactory({
                         devbin: this.devbin
-                    });
+                    }) : settingsAsyncFactory;
                     const publicableSettings = this.constructor.removeNullPropertiesFromObject({
                         env: settingsData.env || null,
                         instrumentalize: settingsData.instrumentalize || null,
-                        traceExternalSources: settingsData.traceExternalSources || null
+                        traceExternalSources: settingsData.traceExternalSources || null,
+                        sectionsMap: settingsData.sectionsMap || null
                     });
                     const publicableJson = this.devbin.compiler.fullpathOf("@/dist/www/dev/settings/publicable.json");
+                    await this.ensureDirectoryOf(publicableJson);
                     await fs.promises.writeFile(publicableJson, JSON.stringify(publicableSettings, null, 2), "utf8");
                 } catch (error) {
                     console.log("[!] Error loading settings:", error);
